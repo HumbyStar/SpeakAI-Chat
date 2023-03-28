07aa7989239ae03f44aac30f2f314373681e1366
Humberto <betogrt300@gmail.com>
2023-03-28 15:17:13 -0300

    Organized initial files

---
+++ SpeakAI-Chat/Home/HomeScreen.swift
@@ -0,0 +1,8 @@
+//
+//  HomeScreen.swift
+//  SpeakAI-Chat
+//
+//  Created by Humberto Rodrigues on 28/03/23.
+//
+
+
---
+++ SpeakAI-Chat/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpeakAI-Chat
+//
+//  Created by Humberto Rodrigues on 28/03/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
